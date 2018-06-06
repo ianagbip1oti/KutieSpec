56f74011d9602bec4399a8490c7e6538345aca2f
ianagbip1oti <ianagbip1oti@gmail.com>
2018-06-06 23:45:38 +0000

    Spec discovery

--- src/main/kotlin/com/github/princesslana/kutiespec/KutieSpecEngine.kt
+++ src/main/kotlin/com/github/princesslana/kutiespec/KutieSpecEngine.kt
@@ -1,11 +1,13 @@
 package com.github.princesslana.kutiespec
 
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner
 import mu.KLogging
 import org.junit.platform.engine.EngineDiscoveryRequest
 import org.junit.platform.engine.ExecutionRequest
 import org.junit.platform.engine.TestDescriptor
 import org.junit.platform.engine.TestEngine
 import org.junit.platform.engine.UniqueId
+import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor
 import org.junit.platform.engine.support.descriptor.EngineDescriptor
 
 class KutieSpecEngine : TestEngine {
@@ -17,10 +19,30 @@ class KutieSpecEngine : TestEngine {
     }
 
     override fun discover(discoveryRequest: EngineDiscoveryRequest, id: UniqueId): TestDescriptor {
-        return EngineDescriptor(id, "KutieSpec")
+        val engine = EngineDescriptor(id, "KutieSpec")
+
+        val specs = HashSet<Class<out KutieSpec>>()
+
+        FastClasspathScanner()
+            .matchSubclassesOf(KutieSpec::class.java) { c -> specs.add(c) }
+            .scan()
+
+        logger.info("Discovered ${specs.size} spec(s)")
+
+        specs.map { s -> KutieSpecDescriptor(id, s) }.forEach(engine::addChild)
+
+        return engine
     }
 
     override fun execute(executionRequest: ExecutionRequest) {
         logger.info("executionRequest: $executionRequest")
+    }
+}
+
+class KutieSpecDescriptor(parentId: UniqueId, clz: Class<out KutieSpec>)
+    : AbstractTestDescriptor(parentId.append("spec", clz.getName()), clz.getSimpleName()) {
+
+    override fun getType(): TestDescriptor.Type {
+        return TestDescriptor.Type.CONTAINER
     }
 }
