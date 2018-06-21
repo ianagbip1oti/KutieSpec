989b17ad788510e60b147301131131c68a7e801b
ianagbip1oti <ianagbip1oti@gmail.com>
2018-06-21 01:49:50 +0000

    Add Example ADT

--- src/main/kotlin/com/github/princesslana/kutiespec/KutieSpec.kt
+++ src/main/kotlin/com/github/princesslana/kutiespec/KutieSpec.kt
@@ -1,17 +1,28 @@
 package com.github.princesslana.kutiespec
 
-open class KutieSpec(val block: DescribeBlock.() -> Unit)
+open class KutieSpec(val block: DescribeBlock.() -> Unit) {
+    internal fun toExample(): Example {
+        return DescribeBlock(this::class.simpleName ?: "").also(block).toExample()
+    }
+}
 
-class DescribeBlock {
+class DescribeBlock(val name: String) {
+    val examples: MutableList<Example> = mutableListOf()
+
     fun describe(name: String, block: DescribeBlock.() -> Unit) {
+        examples.add(DescribeBlock(name).also(block).toExample())
     }
 
     fun it(block: ItBlock.() -> Unit) {
+        examples.add(Single(null, { -> block(ItBlock()) }))
+    }
+
+    internal fun toExample(): Example {
+        return Group(name, examples)
     }
 }
 
 class ItBlock {
-
     fun expect(s: Any): To {
         return To()
     }
